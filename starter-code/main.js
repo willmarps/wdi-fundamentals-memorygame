354161c62ed6256154c4f0b3fb8ff564296575c0
Will Marples <willmarples@wills-mbp.home>
2016-07-21 17:37:10 +0100

    javascript board maker added

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,10 +1,31 @@
-var cardOne = "king";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "queen";
+//var cardOne = "king";
+//var cardTwo = "king";
+//var cardThree = "queen";
+//var cardFour = "queen";
 
-if (cardOne === cardTwo) {
-	alert('You found a match!');	
-} else {
-  alert('Sorry, try again.');
+//if (cardOne === cardTwo) {
+//	alert('You found a match!');	
+//} else {
+//  alert('Sorry, try again.');
+//}
+
+
+var board = document.getElementById('game-board');
+
+
+function createBoard() {
+
+  for (var i=0; i<4; i++) {
+
+    
+    var cardAddition = document.createElement('div');
+
+   
+    cardAddition.className = 'card';
+
+   
+    board.appendChild(cardAddition);
+  }
+
 }
+createBoard();
